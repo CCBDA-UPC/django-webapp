f6098c9b0d97e2ed2b7704d229b241bdeadf703f
angeltoribio-UPC-BCN <atgbcn@gmail.com>
2025-03-14 19:45:47 +0100

    first commit

---
+++ form/models.py
@@ -0,0 +1,44 @@
+import boto3
+from django.conf import settings
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Leads():
+
+    def insert_lead(self, name, email, previewAccess):
+        try:
+            dynamodb = boto3.resource('dynamodb',
+                                      region_name = settings.AWS_REGION,
+                                      aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                                      aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                                      aws_session_token=settings.AWS_SESSION_TOKEN )
+            table = dynamodb.Table(settings.CCBDA_SIGNUP_TABLE)
+        except Exception as e:
+            logger.error(
+                'Error connecting to database table: ' + (e.fmt if hasattr(e, 'fmt') else '') + ','.join(e.args))
+            return 403
+        try:
+            response = table.put_item(
+                Item={
+                    'name': name,
+                    'email': email,
+                    'preview': previewAccess,
+                },
+                ReturnValues='ALL_OLD',
+            )
+        except Exception as e:
+            logger.error(
+                'Error adding item to database: ' + (e.fmt if hasattr(e, 'fmt') else '') + ','.join(e.args))
+            return 403
+        status = response['ResponseMetadata']['HTTPStatusCode']
+        if status == 200:
+            if 'Attributes' in response:
+                logger.error('Existing item updated to database.')
+                return 409
+            logger.error('New item added to database.')
+        else:
+            logger.error('Unknown error inserting item to database.')
+
+        return status
